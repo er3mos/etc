5da41100750a7ff75b08884df9eed965a8c6b403
sean <mail@seanmckernan.xyz>
2025-02-24 21:59:17 +0000

    new file:   nixos/flake.lock 	modified:   nixos/flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,19 +1,23 @@
 {
-  description = "A simple NixOS flake";
+  description = "Nixos config flake";
 
   inputs = {
-    # NixOS official package source, using the nixos-24.11 branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # home-manager = {
+    #   url = "github:nix-community/home-manager";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
-    # Please replace my-nixos with your hostname
+    # use "nixos", or your hostname as the name of the configuration
+    # it's a better practice than "default" shown in the video
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+      specialArgs = {inherit inputs;};
       modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
         ./configuration.nix
+        # inputs.home-manager.nixosModules.default
       ];
     };
   };
