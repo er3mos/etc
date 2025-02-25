65a4c75c869542f0979fdaa9dd6738cc4f38de4c
sean <mail@seanmckernan.xyz>
2025-02-25 23:00:21 +0000

    asd

--- nixos/errLib/default.nix
+++
@@ -1,5 +0,0 @@
-# copied this shit, i understand about 46% of it. At best.
-
-{inputs}: let
-  customLib = (import ./default.nix) {inherit inputs;};
-  outputs = inputs.self.outputs
--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,50 +1,29 @@
 {
-  description = "sean's nixos config - testing";
+  description = "Sean's Basic NixOS configuration";
 
-
-  # Declare all dependencies/programs that we want to use inside inputs. 
   inputs = {
-
-    # Sets the nixpkgs channel to unstable. 
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    # Includes home-manager from nix-community, and pins its version to 
-    # nixpkgs/nixos-unstable above.
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
-  
-  
-  outputs = { self, nixpkgs, ... }@inputs: 
-  # `@inputs` will expose the block of code below, to the inputs that you set above.
 
-    let
-      # system setting defaults
-      system = "x86_64-linux";
-      timezone = "Europe/London";
-      locale = "en_GB.UTF-8";
-
-    in
-
-    {
-      nixosConfigurations = {
-        nixos = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          specialArgs = {
-            inherit inputs;
-            inherit system;
-            inherit timezone;
-            inherit locale;
-          };
-
-          modules = [
-            ./system/nixos/configuration.nix
-            inputs.home-manager.nixosModules.default
-          ];
-        };
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      hostname = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./system/nixos/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.jdoe = import ./home-manager/nixos/home.nix;
+            home-manager.extraSpecialArgs = { inherit inputs; };
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+        ];
       };
     };
+  };
 }
