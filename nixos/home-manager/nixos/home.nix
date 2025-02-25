3444470848576c83f6b92cde34349b2f14760eee
sean <mail@seanmckernan.xyz>
2025-02-25 20:34:43 +0000

    why add to git err time just want to test it mr i am tire mister psls

--- nixos/home-manager/nixos/home.nix
+++ nixos/home-manager/nixos/home.nix
@@ -1,6 +1,9 @@
+# default nixos home-manager file
+
 { config, pkgs, ... }:
 
 {
+
   home.username = "sean";
   home.homeDirectory = "/home/sean";
   home.stateVersion = "24.11"; # DONT CHANGE
@@ -24,51 +27,5 @@
     EDITOR = "nvim";
     TERM = "fish";
   };
-
-  	imports = [
-			../packages/bat.nix
-			../packages/direnv.nix
-			../packages/eza.nix
-			../packages/fd.nix
-			../packages/fish.nix
-			../packages/starship.nix
-];
-
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/sean/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-  };
-
-
+  
 }
