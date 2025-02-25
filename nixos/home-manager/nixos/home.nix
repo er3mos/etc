77e1cbcda230ee1d39f13c331528053e9c09cf0b
sean <mail@seanmckernan.xyz>
2025-02-25 20:47:16 +0000

    why add to git err time just want to test it mr i am tire mister psls

--- nixos/home-manager/nixos/home.nix
+++ nixos/home-manager/nixos/home.nix
@@ -29,27 +29,27 @@
   };
   
   imports = [
-			../packages/bat.nix
-			../packages/brave.nix
+			# ../packages/bat.nix
+			# ../packages/brave.nix
 			../packages/direnv.nix
-			../packages/erdtree.nix
+			# ../packages/erdtree.nix
 			../packages/eza.nix
 			../packages/fd.nix
-			../packages/feh.nix
+			# ../packages/feh.nix
 			../packages/fish.nix
 			../packages/helix.nix
 			../packages/htop.nix
-			../packages/megasync.nix
+			# ../packages/megasync.nix
 			../packages/nap.nix
 			../packages/qbittorrent.nix
-			../packages/rename.nix
+			# ../packages/rename.nix
 			../packages/ripgrep.nix
-			../packages/signal-desktop.nix
+			# ../packages/signal-desktop.nix
 			../packages/starship.nix
 			../packages/tree.nix
 			../packages/vlc.nix
 			../packages/wezterm.nix
-			../packages/ytdlp.nix
+			# ../packages/ytdlp.nix
   ];
 
 }
