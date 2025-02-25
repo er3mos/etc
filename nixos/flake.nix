55914c7aabe7f85b1cbbb8c3716e94cbefd25169
sean <mail@seanmckernan.xyz>
2025-02-25 23:04:15 +0000

    modified:   nixos/flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -9,7 +9,7 @@
 
   outputs = inputs@{ nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
-      hostname = nixpkgs.lib.nixosSystem {
+      nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./system/nixos/configuration.nix
